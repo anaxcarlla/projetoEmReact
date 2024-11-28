5be3f3cf145778870aef39d362e834a117548547
anaxcarlla <153243777+anaxcarlla@users.noreply.github.com>
2024-11-28 16:59:23 -0300

    Initial commit

---
+++ desafioEmReact/src/components/inicio/Inicio.jsx
@@ -0,0 +1,14 @@
+import s from './inicio.module.scss'
+import imagem1 from '../../assets/imagem1doacao.png'
+import imagem2 from '../../assets/imagem2doacao.png'
+import imagem3 from '../../assets/imagem3doacao.png'
+import imagem4 from '../../assets'
+export default function Inicio(){
+    return(
+        <main>
+            <section>
+                <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+        </main>
+    )
+}
